6511cb9a3c85ecb8fe7e23d262b1001fc5bbb410
punjabiquest-dashboard` <stani.norbert03@gmail.com>
2025-11-27 03:18:25 +0100

    tailwind.config.js aktualisieren

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,3 +7,14 @@ module.exports = {
   theme: { extend: {} },
   plugins: [],
 }
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
